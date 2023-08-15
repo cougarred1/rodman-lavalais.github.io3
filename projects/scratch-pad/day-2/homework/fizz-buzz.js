c249ddde9372332a21ab80b1adaef51aa56d1beb
Rodman Lavalais <ua3photo@gmail.com>
2023-08-15 20:49:07 +0000

    day 2

--- projects/scratch-pad/day-2/homework/fizz-buzz.js
+++ projects/scratch-pad/day-2/homework/fizz-buzz.js
@@ -12,7 +12,21 @@
 */
 function fizzBuzz() {
     // YOUR CODE GOES BELOW HERE //
+//print numbers 1 to 100 using for loop
+for (let i = 1; i <= 100; i ++){
 
+    //if i is multiple of 3, print Fizz
+    if (i % 3 === 0 && i % 5 === 0){
+        console.log("FizzBuzz");
+        //if i is multiple of 5, print Buzz
+    } else if (i % 3 === 0){
+        console.log("Fizz");
+    } else if (i % 5 === 0){
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
 
 
     // YOUR CODE GOES ABOVE HERE //
