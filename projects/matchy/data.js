a828d1a911676289bf70cb055cb2039160e95e41
Rodman Lavalais <ua3photo@gmail.com>
2023-08-23 16:37:25 +0000

    matchy

--- projects/matchy/data.js
+++ projects/matchy/data.js
@@ -11,28 +11,64 @@
 //////////////////////////////////////////////////////////////////////
 // Step 1 - Object Creation //////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+var animal = {};
 
+//dot notation to give animal a key named species of my choosing
+
+animal.species = "Mammal";
+
+//use bracket notation to add a key called name
+
+animal["name"] = "Rodman";
+
+//using either notation give animal a key called noises, thats's the value of an empty array
+
+animal.noises = [];
+
+//print animal object to the console
+
+console.log(animal);
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Array Creation ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
+var noises = [];
+//bracket array giving it a sound
+noises[0] = 'roar';
+//add another noise to the end
+noises.push('purr');
+//add to beginning
+noises.unshift("growl");
+//bracket notation to add to end again
 
+noises[noises.length] = 'battlecry'
+//console log length
+
+console.log(noises.length);
+//console log last element
+console.log(noises.length - 1);
+//console log whole array
+
+console.log(noises);
 
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Combining Step 1 and 2 ///////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-
+//add noises key on animal to noises array
+animal['noises'] = noises;
+//add another noise to the noise key on animal
+animal.noises.push("hello");
 
 /* *******************************************************************
  * Step 4 - Review
  *
  * 1. What are the different ways you can access properties on objects?
- *
+ * bracket and dot
  * 2. What are the different ways of accessing elements on arrays?
- *
+ * using bracket notation or and .length
  * *******************************************************************
  */
 
@@ -48,12 +84,51 @@
 //////////////////////////////////////////////////////////////////////
 // Step 6 - A Collection of Animals //////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+var animals = [];
+
+animals.push(animal);
+
+var duck = { species: 'duck', name: 'Jerome', noises: ['quack', 'honk', 'sneeze', 'woosh'] }
+
+//push duck into animals
+
+animals.push(duck);
+
+console.log(animals);
+
+var lioness = { species: 'mammal', name: 'dude', noises: ['1', '2', '3', '4'] } 
+
+var bird = { species: 'chicken', name: 'birdy', noises: ['5', '6', '7', '8'] }
+
+animals.push(lioness);
+
+animals.push(bird);
 
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 7 - Making Friends ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+
+var friends = [];
+
+function getRandom(array){
+ return (Math.floor(Math.random() * array.length));
+}
+console.log(getRandom(animals));
+//Using a random index from this function that you just created, get a random animal and add its `name` to `friends`.
+
+friends.push(animals[getRandom(animals)].name);
+
+console.log(friends);
+
+/**
+ * Using **bracket notation**, add the `friends` list as a **property**
+ *  also named `friends` on one of the animals in the `animals` array
+ * 
+
+
+
 
 
 
