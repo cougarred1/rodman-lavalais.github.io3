4ce19f8387950c42ba4e77fc10c064521f7aa5c2
Rodman Lavalais <ua3photo@gmail.com>
2023-09-16 00:08:29 +0000

    lodown

--- lodown/index.js
+++ lodown/index.js
@@ -208,11 +208,235 @@ _.filter = function(array, func){
 
 module.exports.filter = filter;
 
+/**
+ * reject: Function which returns an array for each element tested in our callback that returned false
+ * 
+ * @param {Array}: Each element tested in the array, after iterating
+ * @param{Function}: Callback function used to test each element on the array
+ * @returns{Array}: Returns array that includes each element for which calling the function returned false
+ */
+
+_.reject = function(array, func){
+  //storage array to push expected values
+  let output = [];
+  //iterate through the array
+  for (let i = 0; i < array.length; i++){
+    //if the callback function of the elements returns false
+    if (func(array[i], i, array) === false) {
+      //push those values in storage array
+      output.push(array[i]);
+    }
+  }
+return output
+}
+
+module.exports.reject = reject;
 
 /**
- * map: function does
+ * partition: Function which returns array of sub arrays where the first sub is the array
+ * for values which calling the funciton returned truthy. While the second
+ * array are the values for which calling the function returned falsyA
+ * 
+ * @param {Array}: Iterate through each of the elements in the array seeing if claling the function
+ * results to truthy or falsy
+ * @param{function}: callback function which is our test
+ * @returns{Array}: Array of two sub arrays, one which the function call invlude all truthy values
+ *  while the other are falsy values
+ */
+
+_.partition = function(array, func){
+  let output = [];
+  let arr1 = [];
+  let arr2 = [];
+   output.push(arr1);
+      output.push(arr2);
+      
+    for (let i = 0; i < array.length; i++){
+     
+      if (func(array[i], i, array) === true) {
+        output[0].push(array[i]);
+      } else {
+        output[1].push(array[i]);
+      }
+    }
+    return output;
+  }
+
+module.exports.partition = partition;
+
+/**
+ * map: function which returns new array returning the callback function of every element
  * 
  * @param {array or object} collection: function takes in an array or object
  * @param{function} func: function takes in an input function to be applied to each value in the collection
  * returns {array} collection: function returns an array madee upof the return values from calling input function
  */
+
+_.map = function(collection, func){
+  let output = [];
+  if (Array.isArray(collection)){
+    for (let i = 0; i < collection.length; i++){
+      //use input func to create "modified" version of current value
+      
+      
+      output.push(func(collection[i], i, collection));
+
+
+      //push modified value into
+    }
+  } else {
+    // iterate object
+    for (let key in collection){
+      output.push(func(collection[key], key, collection));
+    }
+      // pass current value, key, and collection into callback function and push output
+  }
+  return output;
+}
+
+module.exports.map = map;
+
+/**
+ * pluck: Function which returns an array containing the value of every key
+ * 
+ * @param {array of objects}: iterate through the array of objects
+ * @param{properties}: Iterate through each key of in every object iteration
+ * @returns{Array}: Returns an array values of every key
+ */
+
+_.pluck = function(array, prop){
+  let output = [];
+  _.map(array, console.log);
+  for (let i = 0; i < array.length; i++){
+    console.log("inside the for loop", array[i])
+    output.push(array[i][prop]);
+    
+    }
+  return output;
+  }
+
+module.exports.pluck = pluck;
+
+/**
+ * every: A function that returns a boolean depending on the if every element passed
+ * the test provided by the function callback
+ * 
+ * @param {array or object}: The function takes in an array or object to iterate through the elements or keys
+ * @param {function}: callback function provided only returning true if every element or value passes
+ * @returns{boolean}: returns a boolean if every item passes the test, and false even if one item fails
+ */
+
+_.every = function(collec, func){
+  if (Array.isArray(collec) && func !== undefined){
+    for (let i = 0; i < collec.length; i++){
+      if (func(collec[i], i, collec) !== true){
+        return false;
+      } 
+    }
+   
+  } else {
+    if (func !== undefined){
+     for (let i in collec){
+     if (func(collec[i], i, collec) !== true){
+      return false;
+     } 
+    } 
+    }
+  } if (func === undefined){
+      for (let i = 0; i < collec.length; i++){
+        if (!collec[i]){
+          return false;
+        } 
+        }
+      }
+      return true;
+}
+
+module.exports.every = every;
+
+/**
+ * some: A function that returns on boolean depending on if atleast one element or value passed
+ * provided by the function call back is true
+ * 
+ * @param {Array or Object}: iterate through an Array or Object deciding if atleast one element or value is true
+ * @param {function}: A function callback that only passes true if atleast one element passes the test
+ * @returns{boolean}: Returns boolean depending if atleast one element or value passes the callback function provided
+ */
+
+_.some = function(collec, func){
+  if (Array.isArray(collec) && func !== undefined){
+    for (let i = 0; i < collec.length; i++){
+      if (func(collec[i], i, collec) === true){
+        return true;
+      } 
+    }
+   
+  } else {
+    if (func !== undefined){
+     for (let i in collec){
+     if (func(collec[i], i, collec) !== true){
+      return true;
+     } 
+    } 
+    }
+  } if (func === undefined){
+      for (let i = 0; i < collec.length; i++){
+        if (collec[i]){
+          return true;
+        } 
+        }
+      }
+      return false;
+  }
+
+module.exports.some = some;
+
+/**
+ * reduce: A function which callback on each element in the array, and return a single value as the accumulator
+ * after iterating and getting the last updated value
+ * 
+ * @param {array}: The array to iterate over using each element
+ * @param{function}: the callback function which puts applies our code block to every element
+ * @param{seed}: Our initial value which is our accumulator, which when not set, is assigned to our first element of array
+ * @returns{Value}: The final result of running the reducer across all elements
+ */
+
+_.reduce = function(array, func, seed){
+  let result;
+  if (seed === undefined){
+    result = array[0];
+    for (let i = 1; i < array.length; i++){
+      result = func(result, array[i], i);
+    }
+  } else {
+    result = seed;
+    for (let i = 0; i < array.length; i++){
+      result = func(result, array[i], i);
+    }
+
+  }
+  return result;
+}
+
+module.exports.reduce = reduce;
+
+/**
+ * extend: Function  used to copy keys fromone object to another 
+ * 
+ * @param {Object}: The first object used as the base where the second object keys will be copied into
+ * @param{Object}: The second object who's keys are brought into the first
+ * @returns{Object}: Updated object, with all other objects keys brought into it
+ */
+
+_.extend = function(){
+  
+  let obs = arguments[0];
+  for (let i = 1; i < arguments.length; i++){
+    for (let key in arguments[i]){
+      obs[key] = arguments[i][key];
+    }
+  }
+   return obs; 
+}
+
+module.exports.extend = extend;
